19f30fb0515609d69d8ff47d8f7bd9db5ba27050
Mr WhiteKidd <austinslater58@gmail.com>
2022-08-04 00:26:28 -0500

    added mui to homepage and added a avi picture to it. Picture is in assets folder

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,26 +1,57 @@
 import React from "react";
+import austinS from "../../Assets/Images/austinS.jpg"
+import CardHeader from "@mui/material/CardHeader";
+import { Box, CardActions, CardContent, CardMedia } from "@mui/material";
+import { ScopedCssBaseline } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+import Card from "@mui/material/Card";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { maxWidth } from "@mui/system";
+// import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+// import { ClassNames } from "@emotion/react";
+
+const theme = createTheme();
 
 export default function Home() {
-  const mystyle = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
-    padding: "10px",
-    fontFamily: "Arial"
-  };
 
   return (
-    <div>
-      <div style={mystyle}>
+    <ThemeProvider theme={theme}>
+      <ScopedCssBaseline>
+        <Box
+          marginTop={3}
+          marginBottom={3}
+          sx={{
+            flexGrow: 1,
+            minHeight: "calc(100vh - 40px)",
+            backgroundColor: "#90E0EF",
+            borderTopLeftRadius: "8%",
+            borderBottomRightRadius: "8%",
+          }}>
+          <Grid container direction="column" spacing={4}>
+            <Grid item xs={6} marginTop={3} alignSelf="center">
+              <Typography variant="h2" sx={{ minWidth: "100%" }}>
+                Austin Slater
+              </Typography>
+            </Grid>
+          </Grid>
         
-      </div>
-      <p style={mystyle}>
-        Welcome to my portfolio. I'm
-        currently enrolled in a full-stack web development course to increase my
-        knowledge, so I made this portfolio from scratch using react. This
-        portfolio displays my 'highlight reel' of coding programs for the world
-        to see. Without further ado, navigate through the tabs to get to know a
-        little about me!
-      </p>
-    </div>
+          <Card sx={{ mx: "3rem" }} style={{ width: "29rem" }}>
+          <CardMedia
+              component="img"
+              height="300"
+              image={austinS}
+              alt="austin-slater"
+            />
+            <CardContent>
+            <Typography variant="h4">
+            Hello, and welcome to my personal page! I'm an aspiring full-stack developer continuing to upgrade this portfolio as my strength in coding becomes more polished. Please navigate through my from-scratch work of art and don't forget to contact for pointers!
+          </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+      </ScopedCssBaseline>
+    </ThemeProvider>
   );
 }
